b4552c22e13b1d6830221b449559d25477fd41cd
jspieckermann <jspieckermann@web.de>
2021-09-28 16:07:39 +0200

    Create data flow of components

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../model/model';
 
 @Component({
   selector: 'app-book-card',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookCardComponent implements OnInit {
 
+  @Input() item: Item = {} as Item;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from '../model/model';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+  
+  result: SearchResult = {} as SearchResult;
 
-  constructor() { }
+  constructor(private bookService: BookService) {
+    this.bookService.currentResult.subscribe((data: SearchResult) => this.result = data);
+  }
 
   ngOnInit(): void {
   }
