da3b805e69d36a218d2fde526791a9353acbc6cc
jspieckermann <jspieckermann@web.de>
2021-09-28 11:52:05 +0200

    Integrate Book API

--- src/app/book-search/book-search.component.ts
+++ src/app/book-search/book-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from '../model/model';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-book-search',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookSearchComponent implements OnInit {
 
-  constructor() { }
+  result: SearchResult = {} as SearchResult;
 
+  constructor(private searchService: SearchService) {
+    this.searchService.getBookByIsbn('1234').subscribe((data: SearchResult) => this.result = data);
+  }
+    
   ngOnInit(): void {
   }
 
